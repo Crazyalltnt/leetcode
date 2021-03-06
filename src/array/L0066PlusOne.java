ac7f0875d799bf6eeab883477f3865a439d6a636
Crazyalltnt <ningtaook.66@gmail.com>
2021-03-06 13:46:08 +0800

    No.66 加一

---
+++ src/array/L0066PlusOne.java
@@ -0,0 +1,50 @@
+package array;
+
+import java.util.Arrays;
+
+/**
+ * No.66 加一
+ * https://leetcode-cn.com/problems/plus-one/
+ * <p>
+ * 给定一个由 整数 组成的 非空 数组所表示的非负整数，在该数的基础上加一。
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储单个数字。
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * <p>
+ * 示例 1：
+ * 输入：digits = [1,2,3]
+ * 输出：[1,2,4]
+ * 解释：输入数组表示数字 123。
+ * <p>
+ * 示例 2：
+ * 输入：digits = [4,3,2,1]
+ * 输出：[4,3,2,2]
+ * 解释：输入数组表示数字 4321。
+ * <p>
+ * 示例 3：
+ * 输入：digits = [0]
+ * 输出：[1]
+ *
+ * @author Neil
+ * @version v1.0
+ * @date 2021/3/6 13:25
+ */
+public class L0066PlusOne {
+    public static void main(String[] args) {
+        // int[] digits = {1, 2, 3};
+        int[] digits = {9, 9, 9};
+        System.out.println(Arrays.toString(plusOne(digits)));
+    }
+
+    public static int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] = digits[i] % 10;
+            if (digits[i] != 0) {
+                return digits;
+            }
+        }
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
+    }
+}
