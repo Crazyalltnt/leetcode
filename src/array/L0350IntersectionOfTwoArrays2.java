f5ed7713272083d155d97e99812bbf67d8fcd90e
Crazyalltnt <ningtaook.66@gmail.com>
2021-03-17 16:36:26 +0800

    No.350 两个数组的交集II

---
+++ src/array/L0350IntersectionOfTwoArrays2.java
@@ -0,0 +1,116 @@
+package array;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * No.350 两个数组的交集II
+ * https://leetcode-cn.com/problems/intersection-of-two-arrays-ii
+ *
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ *
+ * 示例 1：
+ * 输入：nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出：[2,2]
+ *
+ * 示例 2:
+ * 输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出：[4,9]
+ *
+ * 说明：
+ * 输出结果中每个元素出现的次数，应与元素在两个数组中出现次数的最小值一致。
+ * 我们可以不考虑输出结果的顺序。
+ *
+ * 进阶：
+ * 如果给定的数组已经排好序呢？你将如何优化你的算法？
+ * 如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+ * 如果 nums2 的元素存储在磁盘上，内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+ *
+ * @author Neil
+ * @version v1.0
+ * @date 2021/3/17 15:33
+ */
+public class L0350IntersectionOfTwoArrays2 {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+        System.out.println(Arrays.toString(intersect(nums1, nums2)));
+    }
+
+    /**
+     * 获取交集 使用集合Set
+     * 时间复杂度 O(M+N)
+     * 空间复杂度 O(min(M,N))
+     *
+     * @param nums1 数组1
+     * @param nums2 数组2
+     * @return 公共数组
+     */
+    public static int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            int count = map.getOrDefault(num, 0) + 1;
+            map.put(num, count);
+        }
+
+        int[] intersection = new int[nums1.length];
+        int index = 0;
+        for (int num: nums2) {
+            int count = map.getOrDefault(num, 0);
+            if (count > 0) {
+                intersection[index++] = num;
+                count--;
+                if (count > 0) {
+                    map.put(num, count);
+                } else {
+                    map.remove(num);
+                }
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+
+    /**
+     * 获取交集 使用双指针
+     * 时间复杂度 O(MlogM + NlogN)
+     * 空间复杂度 O(logM + logN)
+     *
+     * @param nums1 数组1
+     * @param nums2 数组2
+     * @return 公共数组
+     */
+    public static int[] intersect2(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int[] intersection = new int[Math.min(nums1.length, nums2.length)];
+
+        int i = 0;
+        int j = 0;
+        int index = 0;
+        while (i < nums1.length && j < nums2.length) {
+            if (nums1[i] == nums2[j]) {
+                intersection[index++] = nums1[i];
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return Arrays.copyOfRange(intersection, 0, index);
+    }
+}
