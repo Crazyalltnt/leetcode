8b11e5e79a74a98e29908eb8997b98081ba4e07f
Crazyalltnt <ningtaook.66@gmail.com>
2021-03-21 23:35:08 +0800

    No.322 零钱兑换

---
+++ src/dp/L0322CoinChange.java
@@ -0,0 +1,80 @@
+package dp;
+
+/**
+ * No.322 零钱兑换
+ * https://leetcode-cn.com/problems/coin-change
+ * <p>
+ * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
+ * 你可以认为每种硬币的数量是无限的。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：coins = [1, 2, 5], amount = 11
+ * 输出：3
+ * 解释：11 = 5 + 5 + 1
+ * <p>
+ * 示例 2：
+ * 输入：coins = [2], amount = 3
+ * 输出：-1
+ * <p>
+ * 示例 3：
+ * 输入：coins = [1], amount = 0
+ * 输出：0
+ * <p>
+ * 示例 4：
+ * 输入：coins = [1], amount = 1
+ * 输出：1
+ * <p>
+ * 示例 5：
+ * 输入：coins = [1], amount = 2
+ * 输出：2
+ *
+ * @author Neil
+ * @version v1.0
+ * @date 2021/3/21 22:14
+ */
+public class L0322CoinChange {
+    public static void main(String[] args) {
+        // int[] coins = {1, 2, 5};
+        // int amount = 11;
+        int[] coins =  {2};
+        int amount = 3;
+        System.out.println(coinChange(coins, amount));
+    }
+
+    static int res = Integer.MAX_VALUE;
+
+    /**
+     * 零钱兑换 暴力递归 超时
+     *
+     * @param coins  硬币种类
+     * @param amount 总金额
+     * @return 硬币最小数量
+     */
+    public static int coinChange(int[] coins, int amount) {
+        if (coins.length == 0) {
+            return -1;
+        }
+
+        findWay(coins, amount, 0);
+
+        // 如果没有任何一种硬币组合能组成总金额，返回 -1。
+        if (res == Integer.MAX_VALUE) {
+            return -1;
+        }
+        return res;
+    }
+
+    public static void findWay(int[] coins, int amount, int count) {
+        if (amount < 0) {
+            return;
+        }
+        if (amount == 0) {
+            res = Math.min(res, count);
+        }
+
+        for (int coin : coins) {
+            findWay(coins, amount - coin, count + 1);
+        }
+    }
+}
