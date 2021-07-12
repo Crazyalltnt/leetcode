14d770ab6817088ac7f17e205396a514c3e6c726
Crazyalltnt <ningtaook.66@gmail.com>
2021-07-12 10:24:27 +0800

    定义链表节点

---
+++ LeetCode-2nd/src/DefinitionDataStructure.java
@@ -0,0 +1,32 @@
+/**
+ * 定义题目内置的各种数据结构
+ *
+ * @author Neil
+ * @version v1.0
+ * @date 2021/7/12 10:21
+ */
+public class DefinitionDataStructure {
+    public static void main(String[] args) {
+
+    }
+}
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
