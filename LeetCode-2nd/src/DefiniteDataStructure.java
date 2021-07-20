b85e38faf3d0269434426d2bba3912b861c4ec2b
Crazyalltnt <ningtaook.66@gmail.com>
2021-07-20 15:54:26 +0800

    Definition for a binary tree node.

--- LeetCode-2nd/src/DefiniteDataStructure.java
+++ LeetCode-2nd/src/DefiniteDataStructure.java
@@ -13,6 +13,10 @@ public class DefiniteDataStructure {
 
 /**
  * Definition for singly-linked list.
+ *
+ * @author Neil
+ * @date 2021/7/12 10:25
+ * @version v1.0
  */
 class ListNode {
     int val;
@@ -28,5 +32,32 @@ class ListNode {
     ListNode(int val, ListNode next) {
         this.val = val;
         this.next = next;
+    }
+}
+
+/**
+ * Definition for a binary tree node.
+ *
+ * @author Neil
+ * @date 2021/7/20 15:50
+ * @version v1.0
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
     }
 }
