fe283ffca559818d21d8f5b9c7c9208e86847794
Crazyalltnt <ningtaook.66@gmail.com>
2021-07-31 18:20:59 +0800

    工具类 创建树

--- LeetCode-2nd/src/DefiniteDataStructure.java
+++ LeetCode-2nd/src/DefiniteDataStructure.java
@@ -1,3 +1,6 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * 定义题目内置的各种数据结构
  *
@@ -59,5 +62,65 @@ class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+}
+
+/**
+ * 工具类
+ *
+ * @author Neil
+ * @date 2021/7/31 18:20
+ * @version v1.0
+ */
+class Util {
+
+    /**
+     * 从数组创建树
+     *
+     * @param nums 数组
+     * @return 树
+     */
+    public static TreeNode createTree(Integer[] nums){
+        if (nums.length == 0) {
+            return new TreeNode(0);
+        }
+        Deque<TreeNode> nodeQueue = new LinkedList<>();
+        // 创建一个根节点
+        TreeNode root = new TreeNode(nums[0]);
+        nodeQueue.offer(root);
+        TreeNode cur;
+        // 记录当前行节点的数量（注意不一定是2的幂，而是上一行中非空节点的数量乘2）
+        int lineNodeNum = 2;
+        // 记录当前行中数字在数组中的开始位置
+        int startIndex = 1;
+        // 记录数组中剩余的元素的数量
+        int restLength = nums.length - 1;
+
+        while(restLength > 0) {
+            for (int i = startIndex; i < startIndex + lineNodeNum; i = i + 2) {
+                // 说明已经将nums中的数字用完，此时应停止遍历，并可以直接返回root
+                if (i == nums.length) {
+                    return root;
+                }
+                cur = nodeQueue.poll();
+                if (nums[i] != null) {
+                    cur.left = new TreeNode(nums[i]);
+                    nodeQueue.offer(cur.left);
+                }
+                // 同上，说明已经将nums中的数字用完，此时应停止遍历，并可以直接返回root
+                if (i + 1 == nums.length) {
+                    return root;
+                }
+                if (nums[i + 1] != null) {
+                    cur.right = new TreeNode(nums[i + 1]);
+                    nodeQueue.offer(cur.right);
+                }
+            }
+            startIndex += lineNodeNum;
+            restLength -= lineNodeNum;
+            lineNodeNum = nodeQueue.size() * 2;
+        }
+
+        return root;
     }
 }
