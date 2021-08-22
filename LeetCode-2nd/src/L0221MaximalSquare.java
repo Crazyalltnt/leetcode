606962f4854da5ed1a3587406d52c256513e87bb
Crazyalltnt <ningtaook.66@gmail.com>
2021-08-22 11:25:31 +0800

    No.221 最大正方形

---
+++ LeetCode-2nd/src/L0221MaximalSquare.java
@@ -0,0 +1,88 @@
+/**
+ * No.221 最大正方形
+ *
+ * @author Neil
+ * @version v1.0
+ * @date 2021/8/22 9:51
+ */
+public class L0221MaximalSquare {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 最大正方形 暴力
+     * 时间复杂度 O(MN * min(M,N)^2)
+     * 空间复杂度 O(1)
+     *
+     * @param matrix 二维矩阵
+     * @return 最大正方形面积
+     */
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    // 遇到一个 1 作为正方形的左上角
+                    maxSide = Math.max(maxSide, 1);
+                    // 计算可能的最大正方形边长
+                    int currentMaxSide = Math.min(rows - i, columns - j);
+                    for (int k = 1; k < currentMaxSide; k++) {
+                        // 判断新增的一行一列是否均为 1
+                        boolean flag = true;
+                        if (matrix[i + k][j + k] == '0') {
+                            break;
+                        }
+                        for (int m = 0; m < k; m++) {
+                            if (matrix[i + k][j + m] == '0' || matrix[i + m][j + k] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag) {
+                            maxSide = Math.max(maxSide, k + 1);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+
+    /**
+     * 最大正方形 动态规划
+     * 时间复杂度 O(MN)
+     * 空间复杂度 O(MN)
+     *
+     * @param matrix 二维矩阵
+     * @return 最大正方形面积
+     */
+    public int maximalSquare2(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+
+        int rows = matrix.length, cols = matrix[0].length;
+        int[][] dp = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == '1') {
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    }
+                    maxSide = Math.max(maxSide, dp[i][j]);
+                }
+            }
+        }
+        return maxSide * maxSide;
+    }
+}
