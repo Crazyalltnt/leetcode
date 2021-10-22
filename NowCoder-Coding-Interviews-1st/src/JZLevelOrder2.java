0d133899d325a5cb1f49bd564d0a512364505b03
Crazyalltnt <ningtaook.66@gmail.com>
2021-10-22 17:33:59 +0800

    剑指 Offer 32 - II. 从上到下打印二叉树 II

---
+++ NowCoder-Coding-Interviews-1st/src/JZLevelOrder2.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 剑指 Offer 32 - II. 从上到下打印二叉树 II
+ *
+ * @author Neil
+ * @version v1.0
+ * @date 2021/10/22 17:05
+ */
+public class JZLevelOrder2 {
+    public static void main(String[] args) {
+
+    }
+
+    public ArrayList<ArrayList<Integer>> Print(TreeNode pRoot) {
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        if (pRoot == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(pRoot);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            ArrayList<Integer> level = new ArrayList<>();
+            while (size-- > 0) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            ans.add(level);
+        }
+        return ans;
+    }
+}
